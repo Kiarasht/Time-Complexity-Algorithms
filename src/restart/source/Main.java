143751ab2d220fe32e802b4e4a86be7f22b5df2c
restart32 <kiarashbikar@gmail.com>
2015-07-17 18:33:35 -0700

    Added mergesort, time comparision added.

--- src/restart/source/Main.java
+++ src/restart/source/Main.java
@@ -1,32 +1,74 @@
 package restart.source;
 
 public class Main {
-    private static int size = 100;
+	private static int size = 10;
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; ++i) {
-            int[] a = randomarray();
-            long startTime = System.currentTimeMillis();
-            //System.out.println(Arrays.toString(a));
-            Selectionsort.selectionsort(a, a.length);
-            //System.out.println(Arrays.toString(a));
-            long endTime = System.currentTimeMillis();
-            long totalTime = endTime - startTime;
-            System.out.println("\nTime for array of size " + size + ": " + totalTime + " milliseconds.");
-            size = size * 10;
-        }
-    }
+	public static void main(String[] args) throws InterruptedException {
+		long totalTime;                                                     // Used to calculate time at each sub section
+		long endTime;
+		long startTime;
+		long Selection = 0;
+		long Merge = 0;
 
-    static int[] randomarray() {
-        int[] a = new int[size];
+		int[] b = randomarray();                                            // This is a redundant call but it helps
+		Selectionsort.selectionsort(b, b.length);                           // the class be loaded before starting
+		System.out.println("Lets sort arrays with 10, 100, 1000, 10000, and 100000 elements.");
+		Thread.sleep(3000);
+		System.out.println("First, Selection.\n");
+		Thread.sleep(3000);
 
-        for (int i = 0; i < a.length; ++i) {
-            a[i] = randomnumber();
-        }
-        return a;
-    }
+		for (int i = 0; i < 5; ++i) {                                       // Do the test on Selectionsort
+			int[] a = randomarray();
+			startTime = System.currentTimeMillis();
+			Selectionsort.selectionsort(a, a.length);
+			endTime = System.currentTimeMillis();
+			totalTime = endTime - startTime;
+			Selection += totalTime;
+			System.out.println("\nTime for array of size " + size + ": " + totalTime + " milliseconds.");
+			size = size * 10;
+		}
+		System.out.println("Total Time: " + Selection + " milliseconds.");
+		size = 10;
 
-    static int randomnumber() {
-        return (int) (((Math.random() * 10) + 1) * size);
-    }
+		System.out.println("\nSelection is by far one of the slowest sorting algorithms. Now lets try a faster one.");
+		Thread.sleep(3000);
+		System.out.println("Second, Merge.\n");
+		Thread.sleep(3000);
+
+		for (int i = 0; i < 5; ++i) {                                       // Do the test on Mergesort
+			int[] a = randomarray();
+			startTime = System.currentTimeMillis();
+			Mergesort.divide(a);
+			endTime = System.currentTimeMillis();
+			totalTime = endTime - startTime;
+			Merge += totalTime;
+			System.out.println("Time for array of size " + size + ": " + totalTime + " milliseconds.");
+			size = size * 10;
+		}
+		System.out.println("Total Time: " + Merge + " milliseconds.");
+		size = 10;
+
+		compare("Selection", Selection, "Merge", Merge);
+	}
+
+	private static void compare(String First, long first, String Second, long second) {
+		if (first > second) {
+			System.out.println("\n" + Second + " was " + first / second + " times faster than " + First + ".");
+		} else {
+			System.out.println("\n" + Second + " was " + second / first + " times slower than " + First + ".");
+		}
+	}
+
+	static int[] randomarray() {                                            // Get a random array of specified size
+		int[] a = new int[size];
+
+		for (int i = 0; i < a.length; ++i) {                                // Get random elements for it
+			a[i] = randomnumber();
+		}
+		return a;
+	}
+
+	static int randomnumber() {
+		return (int) (((Math.random() * 10) + 1) * size);
+	}
 }
