a550b7c86d88db35e60e8f554377e17eaa2884fc
restart32 <kiarashbikar@gmail.com>
2015-07-24 14:02:50 -0700

    Trying to have each sorting algorithm get the same random array, cleaned up a bit.

--- src/restart/source/Main.java
+++ src/restart/source/Main.java
@@ -6,67 +6,67 @@ import java.text.NumberFormat;
 public class Main {
 	private static int size = 10;
 	private static int loop = 5;
+	private static int [][] a = new int[loop][];
+	private static int [][] b = new int[loop][];
 
 	public static void main(String[] args) throws InterruptedException {
-		long totalTime;                                                     // Used to calculate time at each sub section
+		long totalTime;												// Used to calculate time at each sub section
 		long endTime;
 		long startTime;
 		long Selection = 0;
 		long Merge = 0;
 		long Insertion = 0;
 
-		int[] b = randomarray();                                            // This is a redundant call but it helps
-		Selectionsort.selectionsort(b, b.length);                           // the class be loaded before starting
+		for (int i = 0; i < loop; ++i) {
+			a[i] = randomarray();
+			size *= 10;
+		}
+
+		for (int i = 0; i < loop; i++) {
+			b[i] = new int[a[i].length];
+			System.arraycopy(a[i], 0, b[i], 0, a[i].length);
+		}
 
 		System.out.println("First, Selection.\n");
 		Thread.sleep(4000);
 
-		for (int i = 0; i < loop; ++i) {                                    // Do the test on Selectionsort
-			int[] a = randomarray();
+		for (int i = 0; i < loop; ++i) {							// Do the test on Selectionsort
 			startTime = System.currentTimeMillis();
-			Selectionsort.selectionsort(a, a.length);
+			Selectionsort.selectionsort(a[i], a[i].length);
 			endTime = System.currentTimeMillis();
 			totalTime = endTime - startTime;
 			Selection += totalTime;
-			System.out.println("Time for array of size " + size + ": " + totalTime + " milliseconds.");
-			size = size * 10;
+			System.out.println("Time for array of size " + a[i].length + ": " + totalTime + " milliseconds.");
 		}
 		System.out.println("Total Time: " + Selection + " milliseconds.");
-		size = 10;
 
 		System.out.println("\nSecond, Merge.\n");
 		Thread.sleep(4000);
 
-		for (int i = 0; i < loop; ++i) {                                   // Do the test on Mergesort
-			int[] a = randomarray();
+		for (int i = 0; i < loop; ++i) {							// Do the test on Mergesort
 			startTime = System.currentTimeMillis();
-			Mergesort.divide(a);
+			Mergesort.divide(a[i]);
 			endTime = System.currentTimeMillis();
 			totalTime = endTime - startTime;
 			Merge += totalTime;
-			System.out.println("Time for array of size " + size + ": " + totalTime + " milliseconds.");
-			size = size * 10;
+			System.out.println("Time for array of size " + a[i].length + ": " + totalTime + " milliseconds.");
 		}
 		System.out.println("Total Time: " + Merge + " milliseconds.");
-		size = 10;
 
 		System.out.println("\nThird, Insertion.\n");
 		Thread.sleep(4000);
 
-		for (int i = 0; i < loop; ++i) {                                   // Do the test on Insertion
-			int[] a = randomarray();
+		for (int i = 0; i < loop; ++i) {							// Do the test on Insertion
 			startTime = System.currentTimeMillis();
-			Insertionsort.sort(a, a.length);
+			Insertionsort.sort(a[i], a[i].length);
 			endTime = System.currentTimeMillis();
 			totalTime = endTime - startTime;
 			Insertion += totalTime;
-			System.out.println("Time for array of size " + size + ": " + totalTime + " milliseconds.");
-			size = size * 10;
+			System.out.println("Time for array of size " + a[i].length + ": " + totalTime + " milliseconds.");
 		}
 		System.out.println("Total Time: " + Insertion + " milliseconds.");
-		size = 10;
 
-		compare("Selection", Selection, "Merge", Merge);                    // Compare all of them
+		compare("Selection", Selection, "Merge", Merge);			// Compare all of them
 		compare("Insertion", Insertion, "Merge", Merge);
 		compare("Selection", Selection, "Insertion", Insertion);
 	}
@@ -83,10 +83,10 @@ public class Main {
 		}
 	}
 
-	static int[] randomarray() {                                            // Get a random array of specified size
+	static int[] randomarray() {									// Get a random array of specified size
 		int[] a = new int[size];
 
-		for (int i = 0; i < a.length; ++i) {                                // Get random elements for it
+		for (int i = 0; i < a.length; ++i) {						// Get random elements for it
 			a[i] = randomnumber();
 		}
 		return a;
