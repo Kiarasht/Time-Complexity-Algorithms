db6f5ba7ff9535ecb5be04080e96fb458d9b12e6
Restart <torkian@pdx.edu>
2015-09-06 20:50:47 -0700

    Refactoring, removing some inline comments with javadoc instead.

--- src/restart/source/Insertionsort.java
+++ src/restart/source/Insertionsort.java
@@ -1,7 +1,16 @@
 package restart.source;
 
+/**
+ * Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time.
+ * It will go through the list and move an index back until it finds a suitable place for it.
+ */
 public class Insertionsort {
 
+    /**
+     * The main function where the Insertionsort algorithm is defined.
+     * @param a An array that needs to be sorted
+     * @param n The size of the array being passed from Main
+     */
     public static void sort(int[] a, int n) {                       // Insertion implementation
         for (int i = 1; i <= n - 1; ++i) {                          // Find a number and moves it back until
             int x = a[i];                                           // it finds its perfect place
